323a059b4d0c101310193c1bca6dac4beda00afc
yomersaygin <y.omersaygin@gmail.com>
2026-09-05 13:06:37 +0300

    Enable Vivatech Desk stylesheet

--- vivatech_erp/hooks.py
+++ vivatech_erp/hooks.py
@@ -6,3 +6,4 @@ app_email = "dev@localhost"
 app_license = "GPL-3.0-or-later"
 
 after_install = "vivatech_erp.setup.install.after_install"
+app_include_css = "/assets/vivatech_erp/css/vivatech_erp.css"
